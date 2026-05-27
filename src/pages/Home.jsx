4d9685ead3e781c2d7cbd627ff22d4ecac9c321e
aleherrera23 <aleherreraroa23@gmail.com>
2026-05-27 18:35:15 -0500

    Configuracion inicial React y Tailwind

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+
+      <div className="border-2 border-yellow-500 p-10 rounded-2xl shadow-xl">
+
+        <h1 className="text-5xl font-bold text-[#003B7A]">
+          Sistema de Asistencia UAN
+        </h1>
+
+        <p className="mt-4 text-gray-600 text-center">
+          Control de asistencia mediante QR y geolocalización
+        </p>
+
+      </div>
+
+    </div>
+  )
+}
